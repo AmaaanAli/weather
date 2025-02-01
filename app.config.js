ce05ef10c26cea869ba1a59d29acc17eda14444d
Amaan Ali <110116150+AmaaanAli@users.noreply.github.com>
2025-02-01 20:28:16 +0530

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -2,7 +2,17 @@ import "dotenv/config";
 
 export default ({ config }) => ({
   ...config,
+  android: {
+    package: "com.amaanaliy.weather",
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+  },
   extra: {
     API_KEY: process.env.API_KEY,
+    eas: {
+      projectId: "f9e61345-c8cf-45a8-8089-8efe3fbee0f7",
+    },
   },
 });
